c99e408d6f579ea02b95918c130f4950ebb62db8
Ado <adoluna01@gmail.com>
2025-11-22 19:11:32 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          THis is the  <code>CI-CD</code> Demo Projects
+        </p>
+
+          Learn more on React CI/CD Pipeline
+          <p>helllo this is biswajit</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
